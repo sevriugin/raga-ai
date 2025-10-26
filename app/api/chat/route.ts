3a9d6021c25395e43bf9a5133a935ce818c275bd
Sergei Sevriugin <sevriugin@gmail.com>
2025-10-26 17:00:57 +0100

    feat: agents

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -6,11 +6,11 @@ import {
     tool,
     UIMessage,
     stepCountIs,
-    TextPart, generateObject,
+    TextPart,
 } from 'ai';
 import { z } from 'zod';
 import { findRelevantContent } from '@/lib/ai/embedding';
-import { agentRag, agentWeb } from '@/lib/ai/agents';
+import { agentRag, agentWeb, classify } from '@/lib/ai/agents';
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
@@ -49,31 +49,11 @@ export async function POST(req: Request) {
     const query = getUserPrompt(messages);
 
     console.log('[Chat] User prompt: ', query);
-    const model = 'openai/gpt-4o';
 
     /**
      * First step: Classify the query type
      */
-    let classification = 'other';
-    try {
-        const { object: result } = await generateObject({
-            model,
-            output: 'enum',
-            enum: ['personal', 'add', 'web', 'other'],
-            prompt: `Classify the type user query:
-            ${query}
-
-            Determine:
-            1. Query type (personal, add, web, or other)
-                - personal: user is asking about personal information, like "What is my favorite food?";
-                - add: the user provides a random piece of knowledge unprompted, like user said "My favorite food is pizza";
-                - web: user is seeking up-to-date factual information that would be found through a web search, like "What is the current president of the United States?" or "What is the weather in Paris?";
-                - other: user is asking about other information, like "What is the meaning of life?" or "What is the capital of France?";`,
-        });
-        classification = result;
-    } catch (error) {
-        console.error('[Chat] Error during classification:', error);
-    }
+    let classification = await classify(query);
 
     console.log('[Chat] Classification: ', classification);
 
